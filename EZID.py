16a4c95addd7219c85b17b22e0892d7a671251e2
Tom Morrell <tmorrell@caltech.edu>
2017-10-19 10:47:50 -0700

    Update for python3 compatability

--- EZID.py
+++ EZID.py
@@ -165,7 +165,7 @@ class EZIDClient(object):
             print(e.read())
             raise
         output = c.read()
-        if not output.endswith(b"\n"): output += "\n"
+        #if not output.endswith(b"\n"): output += "\n"
         if login:
             output = c.info()["set-cookie"].split(";")[0].split("=")[1]
         return output
@@ -205,7 +205,7 @@ class EZIDClient(object):
         request = urllib.request.Request("%s/id/%s" % (self._server, identifier))
         request.get_method = lambda: "POST"
         request.add_header("Content-Type", "text/plain; charset=UTF-8")
-        request.add_data(formatAnvlFromDict(data).encode("UTF-8"))
+        request.data = formatAnvlFromDict(data).encode("UTF-8")
         return self._get_request(request)
 
     def create(self, identifier, data=None):
@@ -215,7 +215,7 @@ class EZIDClient(object):
         request.get_method = lambda: "PUT"
         if data:
             request.add_header("Content-Type", "text/plain; charset=UTF-8")
-            request.add_data(formatAnvlFromDict(data).encode("UTF-8"))
+            request.data = formatAnvlFromDict(data).encode("UTF-8")
         return self._get_request(request)
 
     def mint(self, shoulder, data=None):
@@ -227,7 +227,7 @@ class EZIDClient(object):
         request.get_method = lambda: "POST"
         if data:
             request.add_header("Content-Type", "text/plain; charset=UTF-8")
-            request.add_data(formatAnvlFromDict(data).encode("UTF-8"))
+            request.data = formatAnvlFromDict(data).encode("UTF-8")
         return self._get_request(request).replace('success: ','').strip()
 
     def delete(self, identifier):
